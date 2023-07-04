589c9c8e20117d793dd6bd5dc9bfad6107cb85e3
Ryan Bigg <me@ryanbigg.com>
2023-07-05 06:32:31 +1000

    Raise an error when no authorization header passed

--- lib/jot/rails/authentication_helper.rb
+++ lib/jot/rails/authentication_helper.rb
@@ -1,7 +1,13 @@
 class Jot
+  NoAuthorizationHeaderError = Class.new(StandardError)
+
   module Rails
     module AuthenticationHelper
       def jot_decode(request)
+        if request.headers["Authorization"].nil?
+          raise NoAuthorizationHeaderError, "no Authorization header detected. Cannot decode token."
+        end
+
         token = request.headers["Authorization"].split(" ").last
         payload, header = Jot.decode(token)
         payload
--- spec/authentication_helper_spec.rb
+++ spec/authentication_helper_spec.rb
@@ -14,34 +14,44 @@ RSpec.describe Jot do
     end
   end
 
-  let(:request) do
-    double(:request, headers: { "Authorization" => "Bearer #{encoded_token}" })
-  end
+  context "with an authorization header" do
+    let(:request) do
+      double(:request, headers: { "Authorization" => "Bearer #{encoded_token}" })
+    end
 
-  let(:expected_payload) { { "user_id" => 1 } }
+    let(:expected_payload) { { "user_id" => 1 } }
 
-  context "with a valid token" do
-    let(:encoded_token) { "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.AB5QoOZ8EYxtqbHd5CTdZOwZjUyHpazJVVCk0zejBAI" }
+    context "with a valid token" do
+      let(:encoded_token) { "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.AB5QoOZ8EYxtqbHd5CTdZOwZjUyHpazJVVCk0zejBAI" }
 
-    it "decodes a token using the configured algorithm and secret" do
-      payload = test_class.new.jot_decode(request)
-      expect(payload).to eq(expected_payload)
+      it "decodes a token using the configured algorithm and secret" do
+        payload = test_class.new.jot_decode(request)
+        expect(payload).to eq(expected_payload)
+      end
+    end
+
+    context "with an invalid token" do
+      let(:encoded_token) { "moo" }
+
+      it "raises an exception with an invalid token" do
+        expect { test_class.new.jot_decode(request) }.to raise_error(JWT::DecodeError)
+      end
+    end
+
+    context "with a manipulated token" do
+      let(:encoded_token) { "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.AB5QoOZ8EYxtqbHd5CTdZOwZjUyHpazJVVCk0zej" }
+
+      it "raises an exception with an invalid token" do
+        expect { test_class.new.jot_decode(request) }.to raise_error(JWT::VerificationError)
+      end
     end
   end
 
-  context "with an invalid token" do
-    let(:encoded_token) { "moo" }
+  context "with no authorization header" do
+    let(:request) { double(:request, headers: {}) }
 
-    it "raises an exception with an invalid token" do
-      expect { test_class.new.jot_decode(request) }.to raise_error(JWT::DecodeError)
-    end
-  end
-
-  context "with a manipulated token" do
-    let(:encoded_token) { "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.AB5QoOZ8EYxtqbHd5CTdZOwZjUyHpazJVVCk0zej" }
-
-    it "raises an exception with an invalid token" do
-      expect { test_class.new.jot_decode(request) }.to raise_error(JWT::VerificationError)
+    it "raises an exception" do
+      expect { test_class.new.jot_decode(request) }.to raise_error(Jot::NoAuthorizationHeaderError)
     end
   end
 end
